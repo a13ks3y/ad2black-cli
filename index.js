8a6d515df35d3af8fddd2e0986e963725ffedbb1
osmyr <osmyr@softserveinc.com>
2018-07-04 15:17:03 +0300

    update logic with using pureimage library, fix unit test.

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 "use strict";
-const convert = (imgToConvert, adImg) => {
-    if (!imgToConvert) {
-        return [];
-    } else if (adImg) {
-        return imgToConvert.map((pixel, index) => {
-            if (adImg[index] && adImg[index] === pixel) {
-                return 0;
-            } else {
-                return pixel;
-            }
+const pi = require('pureimage');
+/**
+ * Remove given array of rectangles with white color in given image (return a copy);
+ * @param imgToConvert {Bitmap}
+ * @param adRects {Array<{x: Number, y: Number, width: Number, height: Number}>}
+ * @returns {Bitmap}
+ */
+const convert = (imgToConvert, adRects) => {
+    const result = pi.make(imgToConvert.width, imgToConvert.height, {});
+    const resultCtx = result.getContext('2d');
+    resultCtx.drawImage(imgToConvert, 0, 0, imgToConvert.width, imgToConvert.height, 0, 0, imgToConvert.width, imgToConvert.height);
+    if (imgToConvert && adRects) {
+        adRects.forEach((rect) => {
+            resultCtx.fillStyle = 'white';
+            resultCtx.fillRect(rect.x, rect.y, rect.width, rect.height);
         });
+        return result;
+
     } else {
-        throw new Error('Expected 2 parameters (arrays of bytes)');
+        throw new Error('Expected 2 parameters (The first is arrays of bytes, and the second is the array of objects)');
     }
 };
+
 if (typeof module !== 'undefined') module.exports = convert;
--- index.test.js
+++ index.test.js
@@ -1,43 +1,49 @@
 "use strict";
 const convert = require('./index');
 const assert = require('assert');
+const pi = require('pureimage');
 const fs = require('fs');
-const getPixels = require('get-pixels');
-const base64 = require('base-64');
+const PNG = require('pngjs').PNG;
+const pixelmatch = require('pixelmatch');
 describe('Index', () => {
     it('Should have a function "convert"', () => {
         assert(convert instanceof Function);
     });
-    it('Should return empty array if no parameters given', () => {
-        assert(convert() instanceof Array);
+    it('Should throw exception if no parameters given', () => {
+        try {
+            // noinspection JSCheckFunctionSignatures
+            convert();
+        } catch (e) {
+            assert(true);
+        }
     });
-    it('Should return an array of pixels with black square instead of given image', () => {
-        getPixels('./test-data/img-to-convert.png',
-            (err, pixels) => {
-                err && (()=>{ console.log("Bad image path"); throw new Error('Bad image path'); })();
-                const imgToConvert = pixels.shape;
-                console.log("imgToConvert", imgToConvert);
-                getPixels('./test-data/img-ad.png', (err, pixels) => {
-                    err && (()=>{ console.log("Bad image path"); throw new Error('Bad image path'); })();
-                    const imgAd = pixels.shape;
-                    console.log('imgAd:', imgAd);
+    it('Should return an image with white square instead of given image', (done) => {
+        pi.decodePNGFromStream(fs.createReadStream('test-data/img-to-convert.png')).then((imgToConvert) => {
+            console.log('imgToConvert', imgToConvert.width, imgToConvert.height);
+            const result = convert(imgToConvert, [{
+                x: 1662,
+                y: 786,
+                width: 237,
+                height: 249
+            }]);
+            pi.decodePNGFromStream(fs.createReadStream('test-data/img-result.png')).then((imgResult) => {
+                pi.encodePNGToStream(result, fs.createWriteStream('test-data/tmp.png')).then(()=>{
+                    const imgResultPngStream = fs.createReadStream('test-data/img-result.png').pipe(new PNG()).on('parsed', doneReading);
+                    const imgTmpPngStream = fs.createReadStream('test-data/tmp.png').pipe(new PNG()).on('parsed', doneReading);
+                    let filesRead = 0;
 
-                    getPixels('./test-data/img-result.png', (err, pixels) => {
-                        err && (()=>{ console.log("Bad image path"); throw new Error('Bad image path'); })();
-                        const imgResult = pixels.shape;
-                        console.log('imgResult:', imgResult);
-                        console.log('imgAd[1].length', imgAd[1]);
-                       assert.deepEqual(convert(imgToConvert, [{
-                           imgToReplaceRect: {
-                               left: 0,
-                               top: imgAd[1],
-                               right: imgAd[0],
-                               bottom: 0
-                           },
-                       }]), imgResult);
-                    });
+                    function doneReading() {
+                        if (++filesRead < 2) return;
+                        const diff = new PNG({width: imgResultPngStream.width, height: imgResultPngStream.height});
+
+                        const diffPixelsCount = pixelmatch(imgResultPngStream.data, imgTmpPngStream.data, diff.data, imgResultPngStream.width, imgResultPngStream.height, {threshold: 0.85});
+                        assert(diffPixelsCount === 0, 'Difference should be 0!');
+                        diff.pack().pipe(fs.createWriteStream('test-data/diff.png'));
+                        console.log('done!');
+                        done();
+                    }
                 });
-            }
-        );
-    });
+            });
+        });
+    }).timeout(10000);
 });
